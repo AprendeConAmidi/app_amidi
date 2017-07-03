d503e096e9a50e5f3cce8c57c8b8076a54280ec1
jonayGodoy <jonaydam2@gmail.com>
2017-07-03 15:33:15 +0100

    refactor

--- integration_test/levelPage.spec.js
+++ integration_test/levelPage.spec.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../src/reducers';
-import initialState from '../src/reducers/initialState';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {mount} from 'enzyme'
 import * as UtilStub from "../src/api/StubApi/UtilStubApi";
-import * as PageSelectorAction from '../src/actions/PageSelectorAction';
 import LevelPage from "../src/components/levelPage/LevelPage";
 
 
