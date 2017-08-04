62fa0cc03b127ac95fbfb8b0fb04bfbf768aeb88
jonay <casa@gmail.com>
2017-08-04 14:59:29 +0100

    button sound is funcionality but missing test

--- src/components/common/template/Template.js
+++ src/components/common/template/Template.js
@@ -3,25 +3,43 @@ import './template.css';
 class Template extends React.Component {
     constructor(props, context) {
         super(props, context);
+
         this.renderChildren = this.renderChildren.bind(this);
+        this.toggleMusic = this.toggleMusic.bind(this);
     }
 
   renderChildren() {/* pattern template */}
+
+
+  getMusic(){
+      if(this.state.isMusic === undefined){
+        this.toggleMusic();
+      }
+
+    if(this.state.isMusic){
+      return(
+        <audio id="audio-bg" preload="auto" autoPlay loop >
+          <source src="../assets/audio/amidi-background.mp3"/>
+          <source src="../assets/audio/amidi-background.ogg"/>
+        </audio>);
+    }
+  }
+
+  toggleMusic(){
+    this.setState(Object.assign({},this.state,{isMusic: !this.state.isMusic}));
+  }
 
   render() {
         return (
           <div>
             <div>
-              <audio id="audio-bg" preload="auto" >
-                <source src="../assets/audio/amidi-background.mp3"/>
-                <source src="../assets/audio/amidi-background.ogg"/>
-              </audio>
+              {this.getMusic()}
               <div className="top-bar">
                   <div className="angle-left-arrow"/>
                   <div className="title-head">
                     <img style={{width: "140px"}} src="../assets/logo.png" alt="Amidi logo"/>
                   </div>
-                  <div className="audio-toggler center-icon muted"/>
+                  <div className={this.state.isMusic ? "audio-toggler center-icon unmuted" : "audio-toggler center-icon muted"}  onClick={this.toggleMusic}/>
               </div>
             </div>
             <div id="children">{this.renderChildren()}</div>
