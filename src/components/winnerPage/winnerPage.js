d71ee0dd3bd6fdc745236ee20216f7edd89a5e2c
jonay <casa@gmail.com>
2017-08-19 13:18:55 +0100

    improve css winnerPage

--- src/components/winnerPage/winnerPage.js
+++ src/components/winnerPage/winnerPage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
 import * as routesPath from '../../routePaths';
+import image_amidi2 from '../../assets/img/amidi2.png';
+import "./winnerPage.css";
 
 export class WinnerPage extends React.Component{
   constructor(props, context) {
@@ -10,9 +12,14 @@ export class WinnerPage extends React.Component{
 
   render() {
     return (
-      <div className="container-fluid">
-        <h1>Página Recuperada</h1>
-        <Link to={routesPath.SELECTOR_PAGE}>Continuar</Link>
+      <div className="box">
+        <img src={image_amidi2} alt="Amidi" className="amidiWinner"/>
+        <h2>¡Felicidades!</h2>
+        <h4>Has ayudado a Amidi a recuperar un poco más de la historia de Canarias</h4>
+        <p>Si quieres puedes seguir ayudándole recuperando más páginas.</p>
+        <div className="cta">
+          <Link to={routesPath.SELECTOR_PAGE}>Continuar</Link>
+        </div>
       </div>
     );
   }
