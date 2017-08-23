aa763d71cb8b1f2f53354842d2f88ba069583885
jonay <casa@gmail.com>
2017-08-23 01:38:12 +0100

    test subdomain "/public"

--- src/routePaths.js
+++ src/routePaths.js
@@ -1,4 +1,4 @@
-export const INDEX = '/';
-export const LEVEL_PAGE = '/category-page';
-export const SELECTOR_PAGE = '/selector-page';
-export const WINNER = '/winner';
+export const INDEX = '/public';
+export const LEVEL_PAGE = INDEX+'/category-page';
+export const SELECTOR_PAGE = INDEX+'/selector-page';
+export const WINNER = INDEX+'/winner';
